4b1641ee7b3797d76c821e69d4555d9253a1eaf4
Madhav Kabra <madhav.kabra1100@gmail.com>
2021-09-18 09:44:28 +0530

    Updates

---
+++ precache-manifest.bece27c3612729bb529261879fa9860d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd2fbe6397ae4735fd9624b0852c851f",
+    "url": "/GmailAutocomplete/index.html"
+  },
+  {
+    "revision": "3938a43a1bb998db2b22",
+    "url": "/GmailAutocomplete/static/css/2.d65a53d6.chunk.css"
+  },
+  {
+    "revision": "5b935ec9802d4805b4dc",
+    "url": "/GmailAutocomplete/static/css/main.de605c20.chunk.css"
+  },
+  {
+    "revision": "3938a43a1bb998db2b22",
+    "url": "/GmailAutocomplete/static/js/2.7e4410c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GmailAutocomplete/static/js/2.7e4410c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b935ec9802d4805b4dc",
+    "url": "/GmailAutocomplete/static/js/main.53d3cdf3.chunk.js"
+  },
+  {
+    "revision": "c97eedcc64649303eb2c",
+    "url": "/GmailAutocomplete/static/js/runtime-main.b56a9614.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/GmailAutocomplete/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/GmailAutocomplete/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/GmailAutocomplete/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/GmailAutocomplete/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/GmailAutocomplete/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
